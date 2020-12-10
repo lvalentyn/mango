2a5bbc5c11d02d7f52bdda42fa4da5691810d6b1
LukashevV <valikallstars@gmail.com>
2020-12-10 22:11:52 +0200

    adaptive part2

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,28 @@
-/*
-* variables
-*/
+const miniNav = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.header__nav');
+    const navLinks = document.querySelectorAll('.header__nav_item');
+    const dark = document.querySelector('.darkbg');
 
-/*
-* test data
-* */
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        dark.classList.toggle('dark');
 
-/*
-* functions dummy calls
-* */
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `burgerFade 0.5s ease forwards ${index / 7 + 0.4}s`;
+            }
+        });
 
-/*
-* main functions
-* */
+        burger.classList.toggle('close');
+    });
+};
+miniNav();
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('.header__top');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
